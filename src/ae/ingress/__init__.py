e18836e2482c068c4140665dd953d366293cce90
Paul Kolesa <chaosandmajesty@gmail.com>
2026-02-05 16:05:57 -0800

    feat: render edge-local ingress bundles

--- src/ae/ingress/__init__.py
+++ src/ae/ingress/__init__.py
@@ -2,5 +2,13 @@
 
 from .caddy import CaddyIngressManager
 from .service import IngressResult, IngressService
+from .edge_local import EdgeLocalIngressConfig, EdgeLocalIngressRenderer, build_edge_local_renderer
 
-__all__ = ["CaddyIngressManager", "IngressService", "IngressResult"]
+__all__ = [
+    "CaddyIngressManager",
+    "IngressService",
+    "IngressResult",
+    "EdgeLocalIngressConfig",
+    "EdgeLocalIngressRenderer",
+    "build_edge_local_renderer",
+]
